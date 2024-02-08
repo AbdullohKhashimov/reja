1a71d7f62edb125adb96e819acb588ee56eab56a
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-02-09 03:20:24 +0900

    feat: train callback hamda async functions

--- train.js
+++ train.js
@@ -20,7 +20,6 @@ console.log(count_letter("e", "engineer"));
 // charAt() = a method that returns the character at a specified index (position) in a string.
 
 // console.log("Jack Ma maslahatlari");
-
 // const list = [
 //   "yaxshi talaba boling", //0-20
 //   "togri boshliq tanlang va koproq hato qiling", // 20-30
@@ -50,5 +49,56 @@ console.log(count_letter("e", "engineer"));
 //     console.log("answer:", data);
 //   }
 // });
-
 // console.log("passed here 1");
+
+// Async function
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 2000);
+//     });
+// setTimeout(() => {
+//   callback(null, list[5]);
+// }, 5000);
+//   }
+// }
+// console.log("passed here 0");
+
+// // callback hell bu joyda biz then\catch ni ishlatdik.
+// maslahatBering(35)
+//   .then((data) => {
+//     maslahatBering(55)
+//       .then((data) => {
+//         console.log("javob:", data);
+//       })
+//       .catch((err) => {
+//         console.log("Error", err);
+//       });
+//     console.log("javob:", data);
+//   })
+//   .catch((err) => {
+//     console.log("Error", err);
+//   });
+
+// // {"synchronous functionlar toliq ishga tushib bulmaguncha asynchronous function ishlamaydi"}
+
+// async\await function call qismda
+// async function run() {
+//   let javob = await maslahatBering(30);
+//   console.log(javob);
+//   javob = await maslahatBering(65);
+//   console.log(javob);
+//   javob = await maslahatBering(45);
+//   console.log(javob);
+// }
+// run();
+
+// console.log("passed here 1"); // bu vaziyatda output asynchronous function oldinda print qiladi chunki bu synchronous.
