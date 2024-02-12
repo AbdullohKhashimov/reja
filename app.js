64cb394cb75fbf28867514f8ff68fb1247037bef
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-02-12 16:49:02 +0900

    feat: mongodb connection

--- app.js
+++ app.js
@@ -1,44 +1,29 @@
+console.log("web server started");
 const express = require("express");
 const app = express();
-const http = require("http");
 
 // MongoDB chaqirish:
 const db = require("./server").db();
 
-// 1: Kirish kodlari
-app.use(express.static("public")); // Bu har qanday browserdan kirib kelayotkan requestlar uchun public folder ochiq degan manoni bildiradi. Faqatgina public folderni kora oliwadi yani clientlar
-app.use(express.json()); // kirib kelayotkan json formatdagi datani object formatga ogirib beradi!
-app.use(express.urlencoded({ extended: true })); // HTML dan traditional request form shakl bor va formdan nmanidur post qisak express qabul qilib oladi
+//1 enter code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// 2: Sessions kodlari
+//2 session code
 
-// 3: Views kodlari
-app.set("views", "views"); //folderni korsatyapmiz
-app.set("view engine", "ejs"); //view enjin ejs ekanligini korsatb beryapmz
+//3 views code
+app.set("views", "views");
+app.set("view engine", "ejs");
 
-// 4: Router kodlari
-app.post("/create-item", function (req, res) {
-  console.log(req.body);
-  const new_reja = req.body.reja;
-  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("Something went wrong");
-    } else {
-      res.end(`successfully added`);
-    }
-  });
+//4 Routing code
+app.post("/create-item", (req, res) => {
+  //   console.log(req.body);
+  //   res.json({ test: "success" });
 });
+
 app.get("/", function (req, res) {
-  db.collection("plans")
-    .find()
-    .toArray((err, data) => {
-      if (err) {
-        console.log(err);
-        res.end("Something went wrong!");
-      } else {
-        console.log(data);
-        res.render("reja");
-      }
-    });
+  res.render("reja");
 });
+
+module.exports = app;
